68cfcf1d62b9364d33927c9c15a2078f153b4ac1
YuichiroHatanaka <73636802+YuichiroHatanaka@users.noreply.github.com>
2020-11-25 00:12:02 +0900

    Update trapezod.c

--- trapezod.c
+++ trapezod.c
@@ -1,24 +1,39 @@
 #include <stdio.h>
 
-int;
-double a, b, c, d, i;
+double a, b, c, d;
+double x, y, i;
+double A, B = 0;
 
-int(main){
-  //式を決定
-  printf("式を決定してください。");
-  scanf("%lf\n", &y)
+//式を決定
+
+
+int main(void){
+  printf("式は y = x*x+2*x-1 です。\n");
   //範囲を決定
   printf("範囲　x0 ~xn　を決定してください。\n");
   printf("x0 = ");
-  scanf("%lf\n", &a);
+  scanf("%lf", &a);
   printf("xn = ");
-  scanf(%lf\n", %b);
+  scanf("%lf", &b);
  //Δxを決定
   printf("Δx　を決定してください。\n");
   printf("Δx = ");
-  scanf("%lf\n", &c);
+  scanf("%lf", &c);
   //計算回数を決定
   d = (b - a + 1)/c;
   //計算
+  for(x = a;x <= b;x = x + c){
+    y = x*x+2*x-1;
+    B = B + y;
+    printf("%lf\n", B);
+  }
+  B = 2*B;
+  x = a;
+  y = x*x+2*x-1;
+  B = B - y;
+  x = b;
+  y = x*x+2*x-1;
+  B = (B - y)/2;
+  printf("答えは%lf。\n", B);
+  return 0;
 }
-  
